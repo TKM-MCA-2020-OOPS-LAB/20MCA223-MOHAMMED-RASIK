2a4e7c5abfee478d007c68f5538772cf8854caa1
Mohammed Rasik <rasikmhd10@gmail.com>
2021-05-10 10:46:33 +0530

    co2 - q4

---
+++ src/com/lab_cycles/co2/q4/Employee.java
@@ -0,0 +1,16 @@
+package com.lab_cycles.co2.q4;
+
+public class Employee {
+
+    public String eNo;
+    public String eName;
+    public int eSalary;
+
+    public Employee(String eNo,String eName,int eSalary)
+    {
+        this.eNo = eNo;
+        this.eName = eName;
+        this.eSalary = eSalary;
+    }
+
+}
