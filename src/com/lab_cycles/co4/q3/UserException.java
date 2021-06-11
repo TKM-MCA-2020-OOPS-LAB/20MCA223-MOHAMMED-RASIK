4656bbc76a89410dbe9579bd085db87d796b9260
Mohammed Rasik <rasikmhd10@gmail.com>
2021-06-12 01:17:17 +0530

    CO4 q3

---
+++ src/com/lab_cycles/co4/q3/UserException.java
@@ -0,0 +1,12 @@
+package com.lab_cycles.co4.q3;
+
+public class UserException extends Exception {
+
+    public UserException(String s)
+    {
+        // Call constructor of parent Exception
+        super(s);
+    }
+
+}
+
