5ccbdbb4f7ae4211b865b73f58ccb5415a2b8f67
Mohammed Rasik <rasikmhd10@gmail.com>
2021-06-12 11:28:07 +0530

    c04 q4

---
+++ src/com/lab_cycles/co4/q4/NegativeNumberException.java
@@ -0,0 +1,12 @@
+package com.lab_cycles.co4.q4;
+
+public class NegativeNumberException extends Exception {
+
+    public NegativeNumberException(String s)
+    {
+        // Call constructor of parent Exception
+        super(s);
+    }
+
+}
+
