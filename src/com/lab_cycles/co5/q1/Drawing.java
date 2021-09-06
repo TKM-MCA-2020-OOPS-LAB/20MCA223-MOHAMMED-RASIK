891f4439f125a4206264f36d1b4888f7fedd6a10
Razik <rasikmhd10@gmail.com>
2021-09-06 13:18:18 +0530

    co5 q1

---
+++ src/com/lab_cycles/co5/q1/Drawing.java
@@ -0,0 +1,14 @@
+import java.applet.Applet;  
+import java.awt.*;  
+  
+public class Drawing extends Applet{  
+  
+public void paint(Graphics g){  
+g.setColor(Color.green);  
+
+g.drawLine(20,30,20,300);  
+g.fillRect(70,100,30,30);  
+g.fillOval(70,200,30,30);  
+  
+}  
+}  
