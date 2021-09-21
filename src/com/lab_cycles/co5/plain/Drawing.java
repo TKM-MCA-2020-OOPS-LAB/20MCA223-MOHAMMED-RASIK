08c6f81752ad1408a48ba2a95ea4a6932426df8e
Mohammed Rasik <rasikmhd10@gmail.com>
2021-09-21 19:03:19 +0530

    CO5 comments

--- src/com/lab_cycles/co5/plain/Drawing.java
+++ src/com/lab_cycles/co5/plain/Drawing.java
@@ -1,9 +1,12 @@
-import java.applet.Applet;  
-import java.awt.Graphics;  
-public class Drawing extends Applet{  
-  
-public void paint(Graphics g){  
-g.drawString("welcome",150,150);  
-}  
-  
-}  
+//import java.applet.Applet;
+//import java.awt.Graphics;
+//public class Drawing extends Applet{
+//
+//public void paint(Graphics g){
+//g.drawString("welcome",150,150);
+//}
+//
+//}
+
+
+// Uncomment the above code in all the co5 programs before running
--- src/com/lab_cycles/co5/q1/Drawing.java
+++ src/com/lab_cycles/co5/q1/Drawing.java
@@ -1,17 +1,17 @@
-package com.lab_cycles.co5.q1;
-
-import java.applet.Applet;  
-import java.awt.*;  
-  
-@SuppressWarnings("deprecation")
-public class Drawing extends Applet{  
-  
-public void paint(Graphics g){  
-g.setColor(Color.green);  
-
-g.drawLine(20,30,20,300);  
-g.fillRect(70,100,30,30);  
-g.fillOval(70,200,30,30);  
-  
-}  
-}  
+//
+//
+//import java.applet.Applet;
+//import java.awt.*;
+//
+//
+//class Drawing extends Applet{
+//
+//public void paint(Graphics g){
+//g.setColor(Color.green);
+//
+//g.drawLine(20,30,20,300);
+//g.fillRect(70,100,30,30);
+//g.fillOval(70,200,30,30);
+//
+//}
+//}
